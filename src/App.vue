40fa012ba41dfd6e1e29052b14e9e0063a21e03a
Aidan McBride <aidankmcbride@gmail.com>
2020-09-10 14:29:02 -0400

    axios requests using online database

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
 import Todos from './components/Todos';
 import Header from './components/layout/Header'
 import AddTodo from './components/AddTodo'
+import axios from 'axios';
 export default {
   name: 'App',
   components: {
@@ -20,32 +21,34 @@ export default {
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        }
-      ]
+      todos: []
     }
   },
   methods: {
     deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res =>  {this.todos = this.todos.filter(todo => todo.id !== id);
+      return res})
+      .catch(err => console.log(err))
+     
     },
     addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
+      // json placeholder gives you an id
+      const { title, completed } = newTodo;
+      axios.post('https://jsonplaceholder.typicode.com/todos/', {
+        title,
+        completed
+      })
+      .then(res => this.todos = [...this.todos, res.data])
+      .catch( err => console.log(err))
+      
     }
+  },
+  // runs right away, similar to componentDidMount()
+  created() {
+     axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+     .then(res => this.todos = res.data)
+     .catch(err => console.log(err))
   }
 }
 </script>
